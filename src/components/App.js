fe3dc9fefcf191077d1a934d05d7bcf891aea2e0
ThisIsItz <itziarurbieta94@gmail.com>
2020-04-06 16:19:50 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class App extends React.Component{
+    state = { resource: 'posts'}
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <button>Posts</button>
+                    <button>Todos</button>
+                </div>
+                    {this.state.resource}
+            </div>
+        )
+    }
+}
+
+export default App
